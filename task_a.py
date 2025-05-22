9a5ccfecd1891732da338312838fa32294e043bb
Jlcarmen <johnluiscarmen.06.jlc@gmail.com>
2025-05-23 07:19:36 +0800

    Initial commit - Task A and D for lab

---
+++ task_a.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+birth_year = input("Enter your birth year: ")
+
+
+print(f"Hello, my name is {name}. I am {age} years old and I was born in {birth_year}.")
